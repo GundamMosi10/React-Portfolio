e5bcef20836282369a4e580bf03ad76fd61c8d02
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-22 16:59:09 -0400

    added an assets folder with my resume and added the download link in the resume page of the app

---
+++ react-portfolio/src/components/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>
+        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
+        molestie urna. 
+      </p>
+    </div>
+  );
+}
