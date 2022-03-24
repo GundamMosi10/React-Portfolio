ca1c8961239325faecc261eaaef9b1a9b61b1bb2
Colin Mosi Cargile <cargile10@gmail.com>
2022-03-24 10:05:19 -0400

    populated the projects with images and links

--- react-portfolio/src/components/pages/Portfolio.js
+++ react-portfolio/src/components/pages/Portfolio.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles/portfolio.css';
+import screenshot from "../../Assets/screenshot.png";
+import screenshotquack from "../../Assets/Screenshot-QUACK.png";
 
 export default function Portfolio() {
   return (
@@ -7,28 +9,28 @@ export default function Portfolio() {
         <div className="card">  
           <div className="face face1">  
             <div className="content">  
-              <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/design_128.png?raw=true"/>  
-              <h3>Design</h3>  
+              <img src={screenshot}/>  
+              <h3>Smart-Shopper</h3>  
             </div>  
           </div>  
           <div className="face face2">  
             <div className="content">  
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>  
-                <a href="#">Read More</a>  
+              <p>SmartShopper is a price comparison website that takes in a user search and returns the top listing from two of the most popular marketplaces on the web - Amazon and eBay. </p>  
+                <a href="https://gundammosi10.github.io/SmartShopper-Application/">Smart Shopper</a>  
             </div>  
           </div>  
         </div>  
         <div className="card">  
           <div className="face face1">  
             <div className="content">  
-              <img src="https://github.com/Jhonierpc/WebDevelopment/blob/master/CSS%20Card%20Hover%20Effects/img/code_128.png?raw=true"/>  
-              <h3>Code</h3>  
+              <img src={screenshotquack}/>  
+              <h3>QUACK</h3>   
             </div>  
           </div>  
           <div className="face face2">  
             <div className="content">  
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cum cumque minus iste veritatis provident at.</p>  
-                <a href="#">Read More</a>  
+              <p>Quack is a social media site where users can write down their thoughts and share them with the world.</p>  
+                <a href="https://quack-social-app.herokuapp.com/">QUACK</a>  
             </div>  
           </div>  
         </div>  
